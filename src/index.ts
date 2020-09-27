146156566b0619a36725515087ba52e3a94e8afe
staszewski <kamilstaszewski95@gmail.com>
2020-09-27 19:29:59 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
 import express from "express"
 import cors from "cors"
 import { createConnection } from "typeorm"
-
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-const PORT = 8000
-
-// app.get("/", (req, res) => res.json({ msg: "Express + TypeScript Server" }))
+import "reflect-metadata";
 
 ;(async () => {
   try {
     await createConnection()
-    console.log("connected to DB")
+    const app = express()
+
+    app.use(cors())
+    app.use(express.json())
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log(`⚡️[server]: Server is running at https://localhost:${process.env.SERVER_PORT}`)
+    })
   } catch (e) {
     console.log(e)
   }
-  // Start web server
-  app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
-  })
 })()
